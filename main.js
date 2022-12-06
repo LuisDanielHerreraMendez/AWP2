97ec9ea36820889138d154be9277cb369244e151
LuisDanielHerreraMendez <75149579+LuisDanielHerreraMendez@users.noreply.github.com>
2022-12-05 23:51:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-//Service Worker
-if('serviceWorker' in navigator) {
-    console.log('Puedes usar el service worker en tu navegador');
+//service worker
+if('serviceWorker' in navigator){
+    console.log('Puedes usar los service worker en tu navegador');
     navigator.serviceWorker.register('./sw.js')
-                            .then(res => console.log('serviceWorker cargador correctamente', res))
-                            .catch(err => console.log('serviceWorker no se ha podido registrar', err))
-} else {
-    console.log('No puede usar los service worker en tu navegador');
+                            .then(res => console.log('serviceWorker cargado correctamente',res))
+                            .catch(err => console.log('serviceWorker no se ha podido usar',err))
+}else{
+    console.log('NO puedes usar los service worker en tu navegador');
 }
+var url = window.location.href;
+var swlocation = 'sw.js'; 
+// Añadir el SW
+if ( navigator.serviceWorker ) {
 
+    if ( url.includes('localhost') ) {
+        swlocation = '/sw.js';
+    }
+
+    
+    navigator.serviceWorker.register(swlocation);
+}
